63ebc977c496b8fd7c808336a5b8bcfa22432ef0
buttonsrtoys <buttonsrtoys@gmail.com>
2022-07-25 17:23:42 -0300

    running again

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:view/registrar.dart';
+import 'package:registrar/registrar.dart';
 
 void main() {
   runApp(const MyApp());
