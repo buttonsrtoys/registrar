08b2c2ac843a9e03754ca178ed4eb3de13fe840e
buttonsrtoys <buttonsrtoys@gmail.com>
2022-09-05 12:47:33 -0300

    fixed test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,7 +5,7 @@ import 'package:example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(myApp());
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('Set Random'), findsOneWidget);
